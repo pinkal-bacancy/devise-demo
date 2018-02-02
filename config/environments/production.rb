3e2b80ca7bf0e6a930adf777f553362498ec165c
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-02-02 12:49:47 +0530

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,7 +85,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-   config.action_mailer.default_url_options = { :host => ENV[:DONAIN] }
+   config.action_mailer.default_url_options = { :host => ENV[:DOMAIN] }
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
